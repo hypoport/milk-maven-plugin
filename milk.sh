e5c2cb3b3b379c0bd5fcb99744b807fbbbd59798
Eric Karge <eric.karge@hypoport.de>
2013-02-11 14:43:26 +0000

    milk.sh: shell script to run any maven goals for modules affected by local changes

---
+++ milk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+WORKING_DIR="$PWD/target"
+CHANGE_SET="$WORKING_DIR/changeSet"
+MODULE_SET="$WORKING_DIR/moduleSet"
+REFERENCE_BRANCH='origin/master'
+
+mkdir -p "$WORKING_DIR"
+
+git diff --name-only "$REFERENCE_BRANCH" > "$CHANGE_SET"
+
+mvn milk:aggregator -DchangeSet="$CHANGE_SET" -DoutputFile="$MODULE_SET"
+mvn -amd -pl "$(<"$MODULE_SET")" "$@"
